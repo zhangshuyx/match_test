6142415e48ac2c4c512ae671f3f5bf983d84933e
ZhangShu <zhangshuyx@gmail.com>
2021-05-28 00:47:59 +0800

    create

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn main() {
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+}
